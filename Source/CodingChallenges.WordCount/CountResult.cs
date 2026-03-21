0e31887448de2e06ef3095bb28bc7f60c684ce80
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-21 05:24:46 +0200

    feat: introduce CountResult record
    Added the core model for tracking line, word, byte and char counts. I made use of C# 14's operator+= overloading, as well as ref semantics to make the API clean.

---
+++ Source/CodingChallenges.WordCount/CountResult.cs
@@ -0,0 +1,26 @@
+﻿namespace CodingChallenges.WordCount;
+
+public record struct CountResult(
+    long Lines = 0,
+    long Words = 0,
+    long Bytes = 0,
+    long Chars = 0)
+{
+    public static CountResult operator +(CountResult left, CountResult right) =>
+        new(
+            left.Lines + right.Lines,
+            left.Words + right.Words,
+            left.Bytes + right.Bytes,
+            left.Chars + right.Chars
+        );
+
+    public void operator += (CountResult right)
+    {
+        Lines += right.Lines;
+        Words += right.Words;
+        Bytes += right.Bytes;
+        Chars += right.Chars;
+    }
+
+    public static readonly CountResult Empty = default;
+}
