c3ab92aea6a7a2cd68550fa754c02c86d9c9eaee
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-21 10:12:38 +0200

    refactor: add constructor to Options

--- Source/CodingChallenges.WordCount/Options.cs
+++ Source/CodingChallenges.WordCount/Options.cs
@@ -2,6 +2,18 @@
 
 public record Options
 {
+    public Options()
+    {
+    }
+
+    public Options(bool lines, bool words, bool chars, bool bytes)
+    {
+        Lines = lines;
+        Words = words;
+        Chars = chars;
+        Bytes = bytes;
+    }
+
     public bool Lines { get; init; }
     public bool Words { get; init; }
     public bool Bytes { get; init; }
