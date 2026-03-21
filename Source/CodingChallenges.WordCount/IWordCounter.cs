3ad996452d7c650e211fe4c57dcb63b507cfccba
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-21 10:48:15 +0200

    feat: implement WordCounter skeleton

---
+++ Source/CodingChallenges.WordCount/IWordCounter.cs
@@ -0,0 +1,8 @@
+﻿using System.IO.Pipelines;
+
+namespace CodingChallenges.WordCount;
+
+public interface IWordCounter
+{
+    Task<CountResult> CountAsync(Stream stream, Options options, CancellationToken ct = default);
+}
---
+++ Source/CodingChallenges.WordCount/WordCounter.cs
@@ -0,0 +1,15 @@
+﻿namespace CodingChallenges.WordCount;
+
+public class WordCounter : IWordCounter
+{
+    public async Task<CountResult> CountAsync(Stream stream, Options options, CancellationToken ct = default)
+    {
+        await Task.Yield();
+        return new CountResult(
+            Lines: -1,
+            Words: -1,
+            Bytes: -1,
+            Chars: -1
+        );
+    }
+}
