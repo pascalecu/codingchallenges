d66da1983a8b644b588da41013d0cde82245f5d0
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-21 03:11:06 +0200

    feat: add Options record to represent parsing flags and files

---
+++ Source/CodingChallenges.WordCount/Options.cs
@@ -0,0 +1,11 @@
+﻿namespace CodingChallenges.WordCount;
+
+public record Options
+{
+    public bool Lines { get; init; }
+    public bool Words { get; init; }
+    public bool Bytes { get; init; }
+    public bool Chars { get; init; }
+    
+    public List<string> Files { get; init; } = [];
+}
