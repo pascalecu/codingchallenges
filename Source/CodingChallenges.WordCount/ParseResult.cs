b95757d6054172550cf695309edfccbc89b43031
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-03-21 03:11:20 +0200

    feat: add ParseResult type to represent parser outcomes

---
+++ Source/CodingChallenges.WordCount/ParseResult.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingChallenges.WordCount;
+
+public class ParseResult
+{
+    public Options? Options { get; private init; }
+    public bool ShowHelp { get; private init; }
+    public string? ErrorMessage { get; private init; }
+
+    public bool IsSuccess => ErrorMessage is null && !ShowHelp && Options is not null;
+
+    public static ParseResult Success(Options options) => new() { Options = options };
+    public static ParseResult Help() => new() { ShowHelp = true };
+    public static ParseResult Failure(string message) => new() { ErrorMessage = message };
+}
